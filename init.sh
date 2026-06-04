aadbfd1e97b0a268d9c91bb6bf3127d3e08af61c
antoineverdon <antoineverdon44240@gmail.com>
2026-06-04 20:28:12 +0000

    update  : init

--- init.sh
+++ init.sh
@@ -4,10 +4,9 @@ echo "Création du dossier Continue..."
 mkdir -p ~/.continue
 
 echo "Génération du fichier config.yaml..."
-# Les guillemets simples autour de 'EOF' sont vitaux.
-# Ils disent au script d'écrire le texte "${LLM_API_KEY}" tel quel,
-# sans essayer de le remplacer.
-cat << 'EOF' > ~/.continue/config.yaml
+# ATTENTION : Il n'y a plus de guillemets autour de EOF !
+# Bash va prendre la valeur de ton Vault et l'écrire en dur dans le fichier.
+cat << EOF > ~/.continue/config.yaml
 name: SSPCloud Config
 version: 1.0.0
 schema: v1
